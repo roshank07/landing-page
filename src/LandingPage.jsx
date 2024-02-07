06dc2f7f79ed0cfd91e10b26ce435d10e96b272b
roshank07 <roshann.purbey@gmail.com>
2024-02-08 00:13:07 +0545

    Completed the final layout

---
+++ src/LandingPage.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+
+const landing_page=()=>{
+    return (
+<div>
+      <div className="sticky top-0 z-50">
+      <div >
+    <div className="w-full bg-black  opacity-90 h-20 flex justify-between ">
+        <div className="w-full lg:w-30/6 xl:w-full  h-full flex items-center px-4 ">          
+              <img className="rounded-lg w-32" src="https://i.ibb.co/tQqTvrz/unipod.png" alt="addify logo" />
+                       
+        </div>
+     
+        <div className="w-full  h-full flex justify-end items-center">
+
+
+
+<a href="https://www.facebook.com/roshan.purbey.07">
+            <button className="relative w-7 h-7 mr-2 focus:outline-none hover:animate-pulse rounded-full  flex items-center justify-center">
+            <svg className="h-10 w-10 text-white" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M48 24a24 24 0 1 0-27.75 23.7V30.95h-6.1V24h6.1v-5.29c0-6.01 3.58-9.34 9.07-9.34 2.62 0 5.37.47 5.37.47v5.91h-3.03c-2.98 0-3.91 1.85-3.91 3.75V24h6.66l-1.07 6.94h-5.59V47.7A24 24 0 0 0 48 24Z"
+                    fill="currentColor"></path>
+            </svg>
+            </button>
+      </a>
+&nbsp;
+
+
+        </div>
+</div>
+  </div>
+      </div>
+
+
+<aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+  <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+    <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+      <h2 className="text-4xl font-bold sm:text-3xl">
+      Gadget paradise awaits! From sleek smartphones to powerful gadgets, our e-commerce store has it all.
+        <span className="hidden sm:block text-2xl">
+          Register now
+        </span>
+      </h2>
+
+      <a href="https://techamigos.online" className="inline-flex items-center px-6 py-3 font-medium bg-blue-600 text-white rounded-full hover:bg-blue-700 transition duration-300 ease-in-out">
+      <svg fill="currentColor" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fillRule="evenodd" clipRule="evenodd">
+        <path d="M4 20h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zm1-12h6v6H5V8zm7 2h6v4h-6v-4z" fill="#fff"/>
+      </svg>
+      <span className="ml-2">Register Now</span>
+      </a>
+    </div>
+  </div>
+  <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+    <img className="w-96" src="https://media.istockphoto.com/id/1277391277/vector/male-character-negative-user-experience-online-review-displeased-client-leave-bad-feedback.jpg?s=1024x1024&w=is&k=20&c=eE0fJ7cvray2UkKz4tOKckRZrflBGmp9JOLbx3gb1uU="
+     alt="couldn't load" style={{width:"100vh"}}/>
+  </div>
+</aside>
+
+<footer className="text-center">
+        <hr />
+        <p className="text-center py-5">
+  Crafted with ❤️ by <span className="font-black"><a href="https://www.facebook.com/roshan.purbey.07" className="text-blue-500 hover:underline">@roshan.purbey.07</a></span>
+</p>
+
+</footer>
+
+
+{/* <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="sahilnetic" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script> */}
+    </div>
+)};
+
+export default landing_page;
